f2b71c7a136a6255400512a8fea14441f2c1e86b
winder3 <39711286+winder3@users.noreply.github.com>
2022-04-15 19:41:30 -0600

    All time zone
    Time changes should work on all time zone

--- germ/convert.js
+++ germ/convert.js
@@ -1,47 +1,62 @@
 var usaC_node = document.querySelector("#USA input");
 var usaC_time = document.querySelector("#USA input").value;
 var germany_node = document.querySelector("#Germany input");
-var germany_time;
-var eastern_node=document.querySelector("#Eastern input");
-var eastern_time;
-var hawaii_node=document.querySelector("#Hawaii input");
-var hawaii_time;
+var germany_time = document.querySelector("#Germany input").value;
+var eastern_node = document.querySelector("#Eastern input");
+var eastern_time = document.querySelector("#Eastern input").value;
+var hawaii_node = document.querySelector("#Hawaii input");
+var hawaii_time = document.querySelector("#Hawaii input").value;
+fromCentral(usaC_time);
+document.addEventListener('change', updateValue);
 
-convertGerm(usaC_time);
-convertEast(usaC_time);
-convertHawaii(usaC_time);
-usaC_node.addEventListener('change', updateValue);
 
-function convertGerm(time){//convert central to germany time
-  var conversion = parseInt(time)+7;
-  if(conversion>=24) //make sure its within the 24 hr boundaries
-  conversion=conversion-24;
-if(conversion<10) //makes 1-9 a time format
-  conversion=String("0"+conversion);
-germany_time = String(conversion+time.substr(2,4));
-germany_node.value = germany_time;
-  console.log(hawaii_node.value);
+function fromCentral(time){//convert times from usa central
+  var conversion = parseInt(time);
+  germany_node.value = toTime(convert(conversion+7),time);//to germany
+  eastern_node.value = toTime(convert(conversion+1),time);//to eastern
+  hawaii_node.value = toTime(convert(conversion+19),time);//to hawaii
 }
-function convertEast(time){//converting usa central to eastern
-  var conversion = parseInt(time)+1;
-  if(conversion>=24) //make sure its within the 24 hr boundaries
-  conversion=conversion-24;
-if(conversion<10) //makes 1-9 a time format
-  conversion=String("0"+conversion);
-  eastern_time= String(conversion+time.substr(2,4));
-  eastern_node.value=eastern_time;
+function fromGermany(time){//converts time to germany
+  var conversion = parseInt(time);
+  usaC_node.value = toTime(convert(conversion+17),time);//to central
+  eastern_node.value = toTime(convert(conversion+18),time);//to eastern
+  hawaii_node.value = toTime(convert(conversion+12),time);//to hawaii
 }
-function convertHawaii(time){//converting usa central to hawaii
-  var conversion = parseInt(time)-5;
-  if(conversion>=24) //make sure its within the 24 hr boundaries
-  conversion=conversion-24;
-if(conversion<10) //makes 1-9 a time format
-  conversion=String("0"+conversion);
-  hawaii_time= String(conversion+time.substr(2,4));
-  hawaii_node.value=hawaii_time;
+function fromEastern(time){
+  var conversion = parseInt(time);
+  usaC_node.value = toTime(convert(conversion+23),time);//to central
+  germany_node.value = toTime(convert(conversion+6),time);//to germany
+  hawaii_node.value = toTime(convert(conversion+18),time);//to hawaii
+}
+function fromHawaii(time){
+  var conversion = parseInt(time);
+  usaC_node.value = toTime(convert(conversion+5),time);//to central
+  eastern_node.value = toTime(convert(conversion+6),time);//to eastern
+  germany_node.value = toTime(convert(conversion+12),time);//to germany
+  
+}
+
+function convert(con){
+  if(con>=24) //make sure its within the 24 hr boundaries
+    con=con%24;
+  if(con<10 & con>=0) //makes 1-9 a time format
+    con=String("0"+con);
+  return con;
+}
+function toTime(num,time){
+  time=String(time);
+  a=String(num+time.substr(2,4));
+  return a;
 }
 function updateValue(e) {
-convertGerm(e.target.value);
-convertEast(e.target.value);
-convertHawaii(e.target.value);
+  
+  console.log(e.target);
+  if(e.target.id=="cent")
+fromCentral(e.target.value);
+  else if(e.target.id=="germ")
+fromGermany(e.target.value);
+  else if(e.target.id=="east")
+fromEastern(e.target.value);
+  else if(e.target.id=="hawa")
+fromHawaii(e.target.value);
 }
